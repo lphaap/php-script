f5fbd3bb0fda47f5873ed0b1956dac91f5979ed2
lphaap <lassi.p.haapala@gmail.com>
2024-03-25 00:21:48 +0200

    feat: Demo page restructuring

--- src/index.php
+++ src/index.php
@@ -2,4 +2,18 @@
 
 define('ROOT_PATH', __DIR__);
 
-require_once(ROOT_PATH . "/public/page.php");
+require_once(ROOT_PATH . "/Router.php");
+
+?>
+
+<div style="padding: 5px;">
+    <a href="/" style>
+        <h1>PHP SCRIPT DEMO</h1></br>
+    </a>
+    <?php
+        $found = Router::get($_SERVER['REQUEST_URI']);
+        if (!$found) {
+    ?>
+            <h1>Not found :(</h1>
+    <?php } ?>
+</div>
---
+++ src/public/wip/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once(ROOT_PATH . "/processor/PHPScript.php");
+use \PScript\PScript;
+
+PScript::require(__DIR__ . "/demo.pscript");
+
+?>
